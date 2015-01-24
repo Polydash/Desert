dae3401b8a02533fbf557ac17347655043d399ba
Polydash <polydash.dev@gmail.com>
2015-01-24 01:39:05 +0100

    Added GameMgr

---
+++ Assets/Scripts/GameMgr/GameMgr.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMgr : MonoBehaviour
+{
+	public static GameMgr s_instance = null;
+
+	private int m_playerID = 0;
+
+	private void Awake()
+	{
+		if(s_instance)
+		{
+			Debug.LogError("Multiple instances of GameMgr");
+		}
+
+		s_instance = this;
+	}
+
+	public int GetNextPlayerID()
+	{
+		return ++m_playerID;
+	}
+}
--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
+	private int m_playerID = 0;
+	private string m_name;
+
+	private void Start()
+	{
+		m_playerID = GameMgr.s_instance.GetNextPlayerID();
+		m_name = "P" + m_playerID.ToString();
+	}
+
 	private void Update()
 	{
-		Debug.Log(new Vector2(Input.GetAxis("P1 Horizontal"), Input.GetAxis("P1 Vertical")));
 	}
 }

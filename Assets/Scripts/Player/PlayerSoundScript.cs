0bac48d1349021363f3eb8296fd037df7cf642d8
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-24 18:08:45 +0100

    Added Steps Sound (alpha test)

---
+++ Assets/Scripts/Player/PlayerSoundScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSoundScript : MonoBehaviour {
+
+    public AudioClip[] stepsSounds;
+    private AudioSource audio;
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        Debug.Log(stepsSounds);
+        Debug.Log(stepsSounds.Length);
+        Debug.Log(stepsSounds[0]);
+        Debug.Log(audio);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponentInChildren<Animator>().GetBool("moving"))
+        {
+            if (!audio.isPlaying)
+            {
+                audio.clip = stepsSounds[Random.Range(0,stepsSounds.Length)];
+                audio.Play();
+            }
+        }
+	}
+}

ebbb1e7fb910cebc65493e165c469f626043cc64
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-25 00:15:36 +0100

    Added In/Out sound at move.

--- Assets/Scripts/Player/PlayerSoundScript.cs
+++ Assets/Scripts/Player/PlayerSoundScript.cs
@@ -4,34 +4,55 @@ using System.Collections;
 public class PlayerSoundScript : MonoBehaviour {
 
     public AudioClip[] stepsSounds;
+    public AudioClip[] inSounds;
+    public AudioClip[] outSounds;
     public float rangeNextStep = 0.25f;
 
-    private AudioSource audioPlayer;
+    private bool wasMoving;
+    private AudioSource[] audioPlayers;
     private float nextStep;
 	// Use this for initialization
 	void Start () {
-        audioPlayer = GetComponent<AudioSource>();
+        audioPlayers = GetComponents<AudioSource>();
         nextStep = 0;
+        wasMoving = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (GetComponentInChildren<Animator>().GetBool("moving"))
         {
+            if(!wasMoving)
+            {
+                audioPlayers[1].clip = inSounds[Random.Range(0, inSounds.Length)];
+                audioPlayers[1].pitch = Random.Range(1.0f, 2.0f);
+                audioPlayers[1].Play();
+            }
             if (nextStep < 0)
             {
-                audioPlayer.clip = stepsSounds[Random.Range(0, stepsSounds.Length)];
-                audioPlayer.pitch = Random.Range(1.0f, 2.0f);
-                audioPlayer.Play();
+                audioPlayers[0].clip = stepsSounds[Random.Range(0, stepsSounds.Length)];
+                audioPlayers[0].pitch = Random.Range(1.0f, 2.0f);
+                audioPlayers[0].Play();
                 nextStep = rangeNextStep;
             }
             else
-            {
+            {   
                 nextStep -= Time.deltaTime;
             }
+            wasMoving = true;
         }
-        else if (audioPlayer.isPlaying)
-            audioPlayer.Stop();
+        else
+        {
+            if (wasMoving)
+            {
+                
+                audioPlayers[0].Stop();
+                audioPlayers[1].clip = outSounds[Random.Range(0, outSounds.Length)];
+                audioPlayers[1].pitch = Random.Range(1.0f, 2.0f);
+                audioPlayers[1].Play();
+            }
+            wasMoving = false;
+        }
 	}
 
     /*void Update()

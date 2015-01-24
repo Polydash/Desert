defbdebd8708c0d86ed9735ae1eaa950f0b3ad2f
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-24 17:21:51 +0100

    Fixed Random Paralax Spawn

--- Assets/Scripts/Paralax/ParalaxItemSpawn.cs
+++ Assets/Scripts/Paralax/ParalaxItemSpawn.cs
@@ -6,14 +6,15 @@ public class ParalaxItemSpawn : MonoBehaviour {
 
     public float nextSpawn = 5.0f;
     public Vector2 spawnRange = new Vector2(5.0f, 15.0f);
-
+    public Vector3 spawnLignePosY = new Vector3(0.6f, 0.9f, 1.2f);
 
     private UnityEngine.GameObject[] ressources;
     private float lastCameraPosition;
+    private GameObject parent;
 	void Start () {
         lastCameraPosition = Camera.main.transform.position.x;
         ressources = Resources.LoadAll<GameObject>("Prefabs/Paralax");
-        Debug.Log(ressources.Length);
+        parent = GameObject.Find("Paralax");
 	}
 	
 	// Update is called once per frame
@@ -23,13 +24,20 @@ public class ParalaxItemSpawn : MonoBehaviour {
         {
             var pos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width + 100, 0, 0));
             int rand = UnityEngine.Random.Range(1,4);
+            float ligneY = spawnLignePosY.x;
+            if (rand == 2)
+                ligneY = spawnLignePosY.y;
+            if (rand == 3)
+                ligneY = spawnLignePosY.z;
+
             GameObject go = GameObject.Instantiate(
                 ressources[UnityEngine.Random.Range(0, ressources.Length)], 
-                new Vector3(pos.x,rand-(0.5f*rand),rand*2 + 2),
+                new Vector3(pos.x,ligneY,rand*2 + 2),
                 new Quaternion()
             ) as GameObject;
-            int scaleSize = new System.Random().Next(-1, 2);
+            int scaleSize = ((int)UnityEngine.Random.Range(0, 2)) * 2 - 1;
             go.transform.localScale = new Vector3((go.transform.localScale.x / rand)*scaleSize, go.transform.localScale.y / rand, go.transform.localScale.z);
+            go.transform.parent = parent.transform;
             nextSpawn = UnityEngine.Random.Range(spawnRange.x, spawnRange.y);
         }
         else

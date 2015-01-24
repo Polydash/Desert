20bd7b64545ae1c26b1202ae36cd9013b7c2a41b
Polydash <polydash.dev@gmail.com>
2015-01-24 15:24:20 +0100

    Modified Event base class

--- Assets/Scripts/Event/EventBase.cs
+++ Assets/Scripts/Event/EventBase.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public abstract class EventBase : MonoBehaviour
 {
-	private Transform[] m_players;
+	private GameObject[] m_players;
+	public Rect m_hitbox;
 
 	public enum eButton
 	{
@@ -23,7 +24,7 @@ public abstract class EventBase : MonoBehaviour
 
 	public Choice[] m_choices; 
 
-	private void Start()
+	protected void Start()
 	{
 		m_players = GameObject.FindGameObjectsWithTag("Player");
 
@@ -44,16 +45,56 @@ public abstract class EventBase : MonoBehaviour
 		}
 	}
 
-	private void Update()
+	protected void Update()
 	{
+		bool shown = false;
+
 		for(int i=0; i<m_players.Length; i++)
 		{
+			float posX = m_players[i].transform.position.x;
+			float posY = m_players[i].transform.position.y;
+			if(posX > m_hitbox.x - m_hitbox.width/2.0f && posX < m_hitbox.x + m_hitbox.width/2.0f &&
+			   posY > m_hitbox.y - m_hitbox.height/2.0f && posY < m_hitbox.y + m_hitbox.height/2.0f)
+			{
+				//TODO(Paul) : Show menu
+				renderer.material.color = Color.red;
+				shown = true;
+			}
+			else
+			{
+				//TODO(Paul) : Hide menu
+				renderer.material.color = Color.white;
+				shown = false;
+			}
+		}
 
+		if(shown)
+		{
+			for(int i=0; i<m_players.Length; i++)
+			{
+				string playerName = "P" + (i+1).ToString() + " ";
+				if(Input.GetButtonDown(playerName + "A"))
+				{
+					DoChoiceA(m_players[i]);
+				}
+				else if(Input.GetButtonDown(playerName + "B"))
+				{
+					DoChoiceB(m_players[i]);
+				}
+				else if(Input.GetButtonDown(playerName + "X"))
+				{
+					DoChoiceX(m_players[i]);
+				}
+				else if(Input.GetButtonDown(playerName + "Y"))
+				{
+					DoChoiceY(m_players[i]);
+				}
+			}
 		}
 	}
 
-	protected override void DoChoiceA(){}
-	protected override void DoChoiceB(){}
-	protected override void DoChoiceX(){}
-	protected override void DoChoiceY(){}
+	protected virtual void DoChoiceA(GameObject player){}
+	protected virtual void DoChoiceB(GameObject player){}
+	protected virtual void DoChoiceX(GameObject player){}
+	protected virtual void DoChoiceY(GameObject player){}
 }

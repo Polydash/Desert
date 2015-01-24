94d854cb499e11a57bf4512c54a41188164e09aa
Polydash <polydash.dev@gmail.com>
2015-01-24 02:14:38 +0100

    Clamped player position to screen

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
+	public float m_speedX;
+	public float m_speedY;
+
 	public int m_playerID{get; set;}
 
 	private string m_name;
@@ -22,6 +25,14 @@ public class PlayerControl : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		rigidbody2D.velocity = m_movement;
+		m_movement = new Vector2(m_movement.x*m_speedX, m_movement.y*m_speedY);
+		rigidbody2D.velocity = m_movement*Time.fixedDeltaTime;
+
+		Vector3 minPos = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f));
+		Vector3 maxPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+
+		transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPos.x, maxPos.x), 
+		                                 Mathf.Clamp(transform.position.y, minPos.y, maxPos.y),
+		                                 transform.position.z);
 	}
 }

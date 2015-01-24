876cad58ea5963d19c100df26ad83da2a2106f21
Polydash <polydash.dev@gmail.com>
2015-01-24 13:21:15 +0100

    Added Event base class

---
+++ Assets/Scripts/Event/EventBase.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventBase : MonoBehaviour
+{
+	public enum eButton
+	{
+		A,
+		B,
+		X,
+		Y
+	}
+	
+	[System.Serializable]
+	public struct Choice
+	{
+		public eButton button;
+		public string text;
+		public Item.eItemTag[] m_tagList;
+	}
+
+	public Choice[] m_choices; 
+
+	private void Start()
+	{
+		if(m_choices.Length > 4)
+		{
+			Debug.LogError("Too many choices on event");
+			return;
+		}
+
+		bool[] buttons = {false, false, false, false};
+		for(int i=0; i<buttons.Length; i++)
+		{
+			if(buttons[(int) m_choices[i].button])
+			{
+				Debug.LogError("Two choices are mapped on the same button");
+			}
+			buttons[(int) m_choices[i].button] = true;
+		}
+	}
+
+	private void Update()
+	{
+		//Map input
+	} 
+}

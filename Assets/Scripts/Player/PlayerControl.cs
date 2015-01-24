4a85cec3d43eb3fdd27ef35cd0e5f037b1100939
Polydash <polydash.dev@gmail.com>
2015-01-24 09:23:20 +0100

    Fixed Player movement

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -5,12 +5,10 @@ public class PlayerControl : MonoBehaviour
 {
 	public float m_speedX;
 	public float m_speedY;
-	public float m_maxSpeed;
 
 	public int m_playerID{get; set;}
 
 	private string m_name;
-	private Vector2 m_movement;
 
 	private void Start()
 	{
@@ -18,10 +16,15 @@ public class PlayerControl : MonoBehaviour
 		m_name = "P" + m_playerID.ToString();
 	}
 
-	private void Update()
+	private void FixedUpdate()
 	{
-		m_movement = new Vector2(Input.GetAxis(m_name + " Horizontal"),
-		                         Input.GetAxis(m_name + " Vertical"));
+		Vector2 movement = new Vector2(Input.GetAxis(m_name + " Horizontal"),
+		                         	   Input.GetAxis(m_name + " Vertical"));
+
+		movement.x *= m_speedX * Time.fixedDeltaTime;
+		movement.y *= m_speedY * Time.fixedDeltaTime;
+
+		rigidbody2D.velocity = movement;
 
 		Vector3 minPos = Camera.main.ScreenToWorldPoint(new Vector3(0.0f, 0.0f));
 		Vector3 maxPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
@@ -29,14 +32,5 @@ public class PlayerControl : MonoBehaviour
 		transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPos.x, maxPos.x), 
 		                                 Mathf.Clamp(transform.position.y, minPos.y, maxPos.y),
 		                                 transform.position.z);
-	}
-
-	private void FixedUpdate()
-	{
-		m_movement = new Vector2(m_movement.x*m_speedX, m_movement.y*m_speedY);
-		rigidbody2D.velocity += m_movement*Time.deltaTime;
-
-		rigidbody2D.velocity = new Vector2(Mathf.Clamp(rigidbody2D.velocity.x, -m_maxSpeed, m_maxSpeed),
-		                                   Mathf.Clamp(rigidbody2D.velocity.y, -m_maxSpeed, m_maxSpeed));
 	}
 }

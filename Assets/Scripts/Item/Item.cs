a1b282f785a73f924106b93bd533fe2fe831e174
Polydash <polydash.dev@gmail.com>
2015-01-24 13:48:32 +0100

    Added item tags

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class Item : MonoBehaviour
 {
-	public string m_name;
 	public enum eItemTag : int
 	{
-		WEAPON = 0x00000001 << 0,
-		HEALING = 0x00000001 << 1,
-		HURTING = 0x00000001 << 2
+		HEALING = 0x00000001 << 0,
+		FOOD = 0x00000001 << 1,
+		WEAPON = 0x00000001 << 2,
+		SEED = 0x00000001 << 3,
+		LIQUID = 0x00000001 << 4,
+		VALUABLE = 0x00000001 << 5,
+		CURRENCY = 0x00000001 << 6,
+		MASK = 0x00000001 << 7,
+		ARTEFACT = 0x00000001 << 8,
+		CONTAINER = 0x00000001 << 9,
+		TOOL = 0x00000001 << 10
 	}
 
 	public eItemTag[] m_tagList;

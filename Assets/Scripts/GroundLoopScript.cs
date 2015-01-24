1b2ca6cad38442fc36d57d8a485c21c37bbc8132
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-24 12:20:11 +0100

    Fixed GroundLoop
    Pas de clip de texture au sol.

--- Assets/Scripts/GroundLoopScript.cs
+++ Assets/Scripts/GroundLoopScript.cs
@@ -47,7 +47,7 @@ public class GroundLoopScript : MonoBehaviour {
                 Vector3 firstChildPos = new Vector3(firstChild.position.x + (firstChild.renderer.bounds.size.x / 2), firstChild.position.y, firstChild.position.x);
                 Vector3 firstChildPosPix = Camera.main.WorldToScreenPoint(firstChildPos);
                 Debug.Log(firstChildPosPix);
-                if (firstChildPosPix.x < 0)
+                if (firstChildPosPix.x <= 0)
                 {
                     // On récupère le dernier élément de la liste
                     Transform lastChild = groundPart.LastOrDefault();

3fc1b26ff957f7d0c42e6e109d2f5b8b10d3d8e2
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-25 17:57:13 +0100

    Added BanditEvent

---
+++ Assets/Scripts/Event/Events/BanditEvent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BanditEvent : EventBase
+{
+
+    protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        Debug.Log(m_data.m_choices[choice].text);
+        if (choice == 0 || choice == 1)
+        {
+            player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+
+        }
+        if (choice >1)
+        {
+            player.GetComponent<PlayerControl>().LoseLife();
+        }
+        Destroy(gameObject);
+    }
+}

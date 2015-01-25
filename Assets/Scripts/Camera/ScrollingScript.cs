8e2a6eb0ffed9b8ef1171bcf878270f5de665dfd
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-25 11:33:53 +0100

    Added CanScoll on ScrollScript

--- Assets/Scripts/Camera/ScrollingScript.cs
+++ Assets/Scripts/Camera/ScrollingScript.cs
@@ -5,7 +5,8 @@ public class ScrollingScript : MonoBehaviour {
 
     [Range(0.0f, 1.0f)]
     public float seuilDeScroll = 0.66f;
-    public int Direction { get { return direction; } } 
+    public int Direction { get { return direction; } }
+    public bool CanScroll { get; set; } 
     private int direction = 0;
     
     private int screenWidth;
@@ -13,97 +14,101 @@ public class ScrollingScript : MonoBehaviour {
     private Vector2 movement = new Vector2(0,0);
 
 	void Start () {
+        CanScroll = false;
         players = GameObject.FindGameObjectsWithTag("Player");
         screenWidth = Screen.width;
 	}
 
     void LateUpdate()
     {
-        if (direction == 0)
+        if (CanScroll)
         {
-            int posPlayers = 0;
-            foreach (GameObject ob in players)
+            if (direction == 0)
             {
-                var screenPos = camera.WorldToScreenPoint(ob.transform.position);
-                if (screenPos.x > seuilDeScroll * screenWidth)
+                int posPlayers = 0;
+                foreach (GameObject ob in players)
                 {
-                    posPlayers++;
-                }
-                else if (screenPos.x < (1 - seuilDeScroll) * screenWidth)
-                {
-                    posPlayers--;
-                }
-            }
-            if (posPlayers == (players.Length))
-                direction = 1;
-            else if (posPlayers == (players.Length * -1))
-                direction = -1;
-            if (direction != 0)
-            {
-                GroundLoopScript groundScript = GameObject.Find("Grounds").GetComponent<GroundLoopScript>();
-                if (groundScript != null)
-                {
-                    groundScript.direction = direction;
-                    groundScript.enabled = true;
-                }
-            }
-        }
-        else
-        {
-            bool move = true;
-            float distance = float.MaxValue;
-            foreach (GameObject ob in players)
-            {
-                var screenPos = camera.WorldToScreenPoint(ob.transform.position);
-                if (direction == 1)
-                {
-                    if (screenPos.x <= seuilDeScroll * screenWidth)
+                    var screenPos = camera.WorldToScreenPoint(ob.transform.position);
+                    if (screenPos.x > seuilDeScroll * screenWidth)
                     {
-                        move = false;
+                        posPlayers++;
                     }
-                    else
+                    else if (screenPos.x < (1 - seuilDeScroll) * screenWidth)
                     {
-                        float newDist =  screenPos.x - (seuilDeScroll * screenWidth);
-                        if (newDist < distance)
-                        {
-                            distance = newDist;
-                        }
+                        posPlayers--;
                     }
                 }
-                else if (direction == -1)
+                if (posPlayers == (players.Length))
+                    direction = 1;
+                else if (posPlayers == (players.Length * -1))
+                    direction = -1;
+                if (direction != 0)
                 {
-                    if (screenPos.x >= (1 - seuilDeScroll) * screenWidth)
+                    GroundLoopScript groundScript = GameObject.Find("Grounds").GetComponent<GroundLoopScript>();
+                    if (groundScript != null)
                     {
-                        move = false;
-                    }
-                    else
-                    {
-                        float newDist = ((1-seuilDeScroll) * screenWidth) - screenPos.x ;
-                        if (newDist < distance)
-                        {
-                            distance = newDist;
-                        }
+                        groundScript.direction = direction;
+                        groundScript.enabled = true;
                     }
                 }
-            }
-            if (move == true)
-            {
-                var posCam = camera.WorldToScreenPoint(camera.transform.position);
-                var mv = camera.ScreenToWorldPoint(new Vector3(posCam.x + (direction*distance), 0.0f, 0.0f));
-                movement = new Vector2(mv.x, 0.0f);
-                
             }
             else
             {
-                movement = new Vector2(transform.position.x, transform.position.y);
-            }
-            float paralaxSpeed = (movement.x - transform.position.x);
-            transform.position = new Vector3(movement.x, transform.position.y, transform.position.z);
-            if (move)
-            {
-                foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Paralax"))
+                bool move = true;
+                float distance = float.MaxValue;
+                foreach (GameObject ob in players)
                 {
-                    obj.transform.position = new Vector3(obj.transform.position.x + paralaxSpeed * (obj.transform.position.z / 10), obj.transform.position.y, obj.transform.position.z);
+                    var screenPos = camera.WorldToScreenPoint(ob.transform.position);
+                    if (direction == 1)
+                    {
+                        if (screenPos.x <= seuilDeScroll * screenWidth)
+                        {
+                            move = false;
+                        }
+                        else
+                        {
+                            float newDist = screenPos.x - (seuilDeScroll * screenWidth);
+                            if (newDist < distance)
+                            {
+                                distance = newDist;
+                            }
+                        }
+                    }
+                    else if (direction == -1)
+                    {
+                        if (screenPos.x >= (1 - seuilDeScroll) * screenWidth)
+                        {
+                            move = false;
+                        }
+                        else
+                        {
+                            float newDist = ((1 - seuilDeScroll) * screenWidth) - screenPos.x;
+                            if (newDist < distance)
+                            {
+                                distance = newDist;
+                            }
+                        }
+                    }
+                }
+                if (move == true)
+                {
+                    var posCam = camera.WorldToScreenPoint(camera.transform.position);
+                    var mv = camera.ScreenToWorldPoint(new Vector3(posCam.x + (direction * distance), 0.0f, 0.0f));
+                    movement = new Vector2(mv.x, 0.0f);
+
+                }
+                else
+                {
+                    movement = new Vector2(transform.position.x, transform.position.y);
+                }
+                float paralaxSpeed = (movement.x - transform.position.x);
+                transform.position = new Vector3(movement.x, transform.position.y, transform.position.z);
+                if (move)
+                {
+                    foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Paralax"))
+                    {
+                        obj.transform.position = new Vector3(obj.transform.position.x + paralaxSpeed * (obj.transform.position.z / 10), obj.transform.position.y, obj.transform.position.z);
+                    }
                 }
             }
         }
--- Assets/Scripts/Player/PlayerAnimScript.cs
+++ Assets/Scripts/Player/PlayerAnimScript.cs
@@ -5,30 +5,45 @@ public class PlayerAnimScript : MonoBehaviour {
 
     public float seuil = 0.01f;
     private bool right = true;
+    private PlayerControl playerScript;
 	// Use this for initialization
 	void Start () {
-	    
+        playerScript = GetComponent<PlayerControl>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Mathf.Abs(rigidbody2D.velocity.x) > seuil || Mathf.Abs(rigidbody2D.velocity.y) > seuil)
+        if (Input.GetButton("P" + playerScript.m_playerID + " A") || Input.GetButton("P" + playerScript.m_playerID + " B"))
         {
-            if (rigidbody2D.velocity.x < 0 && right)
-            {
-                Vector3 scale = GetComponentInChildren<Transform>().localScale;
-                GetComponentInChildren<Transform>().localScale = new Vector3(-scale.x, scale.y, scale.z);
-                right = false ;
-            }
-            else if( rigidbody2D.velocity.x > 0 && !right)
-            {   
-                Vector3 scale = GetComponentInChildren<Transform>().localScale;
-                GetComponentInChildren<Transform>().localScale = new Vector3(-scale.x, scale.y, scale.z);
-                right = true ;
-            }
-            GetComponentInChildren<Animator>().SetBool("moving", true);
+            GetComponentInChildren<Animator>().SetBool("moving", false);
+            GetComponentInChildren<Animator>().SetBool("using", true);
         }
         else
-            GetComponentInChildren<Animator>().SetBool("moving", false);
+        {
+            GetComponentInChildren<Animator>().SetBool("using", false);
+        }
+        if (Mathf.Abs(rigidbody2D.velocity.x) > seuil || Mathf.Abs(rigidbody2D.velocity.y) > seuil)
+            {
+                if (rigidbody2D.velocity.x < 0 && right)
+                {
+                    Vector3 scale = GetComponentInChildren<Transform>().localScale;
+                    GetComponentInChildren<Transform>().localScale = new Vector3(-scale.x, scale.y, scale.z);
+                    right = false;
+                }
+                else if (rigidbody2D.velocity.x > 0 && !right)
+                {
+                    Vector3 scale = GetComponentInChildren<Transform>().localScale;
+                    GetComponentInChildren<Transform>().localScale = new Vector3(-scale.x, scale.y, scale.z);
+                    right = true;
+                }
+                GetComponentInChildren<Animator>().SetBool("using", false);
+                GetComponentInChildren<Animator>().SetBool("moving", true);
+
+            }
+            else
+            {
+                GetComponentInChildren<Animator>().SetBool("moving", false);
+            }
+        
 	}
 }

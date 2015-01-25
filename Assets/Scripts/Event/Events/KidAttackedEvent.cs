e6c07de112d2880d8894b94290129e3b6ef9cc3a
Polydash <polydash.dev@gmail.com>
2015-01-25 18:10:07 +0100

    Added Kid attacked event

---
+++ Assets/Scripts/Event/Events/KidAttackedEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KidAttackedEvent : EventBase
+{
+    protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        switch(choice)
+        {
+            case 0 :
+                player.GetComponent<PlayerControl>().LoseLife();
+                renderer.material.color = Color.grey;
+                transform.GetChild(0).renderer.enabled = false;
+                this.enabled = false;
+                break;
+
+            case 1 :
+                player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+                renderer.material.color = Color.grey;
+                transform.GetChild(0).renderer.enabled = false;
+                this.enabled = false;
+                break;
+        }
+    }
+}

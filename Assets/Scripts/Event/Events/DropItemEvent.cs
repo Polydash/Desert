c4e7de60b2ee9e4ca760c0416a73aa9a3d60e8d9
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-25 15:48:13 +0100

    Added DropEvent

--- Assets/Scripts/Event/Events/DropItemEvent.cs
+++ Assets/Scripts/Event/Events/DropItemEvent.cs
@@ -1,21 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TreeEvent : EventBase {
+public class DropItemEvent : EventBase {
 
+    public Transform m_item;
     protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
     {
         GameObject itemEvent = Resources.Load<GameObject>("Prefabs/Events/ItemEvent");
-        Item baton = Resources.Load<Item>("Prefabs/Items/Spear");
-        if (!baton)
+        Item it = m_item.GetComponent<Item>(); ; //Resources.Load<Item>("Prefabs/Items/Spear");
+        if (!it)
         {
             Debug.LogError("Failed to load items");
         }
 
         //itemEvent.m_item = baton;
         GameObject ie = GameObject.Instantiate(itemEvent, new Vector3(transform.position.x, transform.position.y - (renderer.bounds.size.y / 2), transform.position.z), new Quaternion()) as GameObject;
-        (ie.renderer as SpriteRenderer).sprite = (baton.renderer as SpriteRenderer).sprite;
-        ie.GetComponent<ItemEvent>().m_item = baton;
+        (ie.renderer as SpriteRenderer).sprite = (it.renderer as SpriteRenderer).sprite;
+        ie.GetComponent<ItemEvent>().m_item = it;
         
         renderer.material.color = Color.gray;
 

6ffc47e5cb87a7fd03d22e81633efda307012cbf
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-25 16:45:18 +0100

    Added YourteEvent

--- Assets/Scripts/Event/Events/AutelEvent.cs
+++ Assets/Scripts/Event/Events/AutelEvent.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GainLifeEvent : EventBase {
+public class AutelEvent : EventBase {
 
     protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
     {
         Debug.Log(m_data.m_choices[choice].text);
         player.GetComponent<PlayerControl>().GainLife();
+        
         player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
 
         GetComponent<Animator>().SetBool("used", true);
+        
         this.enabled = false;
     }
 	
---
+++ Assets/Scripts/Event/Events/YourteEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YourteEvent : EventBase {
+
+    protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        Debug.Log(m_data.m_choices[choice].text);
+        player.GetComponent<PlayerControl>().GainLife();
+        renderer.material.color = Color.gray;
+        this.enabled = false;
+    }
+}

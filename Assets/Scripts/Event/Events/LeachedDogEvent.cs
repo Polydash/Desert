3b04dcb872381d029fc680139058a276705c4a9c
Polydash <polydash.dev@gmail.com>
2015-01-25 17:18:55 +0100

    Added Leached dog event

---
+++ Assets/Scripts/Event/Events/LeachedDogEvent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeachedDogEvent : EventBase
+{
+	protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        switch(choice)
+        {
+            case 0 :
+                Destroy(gameObject);
+                break;
+
+            case 1 :
+                GameObject itemEvent = Resources.Load<GameObject>("Prefabs/Events/ItemEvent");
+                GameObject meat = Resources.Load<GameObject>("Prefabs/Items/Raw Meat");
+                Item it = meat.GetComponent<Item>();
+                if(!it)
+                {
+                    Debug.LogError("Failed to load items");
+                }
+                player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+                GameObject ie = GameObject.Instantiate(itemEvent, new Vector3(transform.position.x, transform.position.y - (renderer.bounds.size.y / 2), transform.position.z), new Quaternion()) as GameObject;
+                (ie.renderer as SpriteRenderer).sprite = (it.renderer as SpriteRenderer).sprite;
+                ie.GetComponent<ItemEvent>().m_item = it;
+                Destroy(gameObject);
+                break;
+
+            case 2 :
+                player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+                Destroy(gameObject);
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+}

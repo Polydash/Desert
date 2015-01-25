39c8a0d76052e2aba1d499cfd084f492c1d5f4c4
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-25 16:36:31 +0100

    Added AutelEvent

---
+++ Assets/Scripts/Event/Events/GainLifeEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GainLifeEvent : EventBase {
+
+    protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        Debug.Log(m_data.m_choices[choice].text);
+        player.GetComponent<PlayerControl>().GainLife();
+        player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+
+        GetComponent<Animator>().SetBool("used", true);
+        this.enabled = false;
+    }
+	
+}

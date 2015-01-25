d7a8f857b2ac9687798bb8c130ef7a2354ee5c4b
Polydash <polydash.dev@gmail.com>
2015-01-25 16:29:12 +0100

    Added Events

---
+++ Assets/Scripts/Event/Events/BonfireEvent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonfireEvent : EventBase
+{
+    protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        switch(choice)
+        {
+            case 0 :
+                GameObject itemEvent = Resources.Load<GameObject>("Prefabs/Events/ItemEvent");
+                GameObject meat = Resources.Load<GameObject>("Prefabs/Items/Meat");
+                Item it = meat.GetComponent<Item>();
+                if (!it)
+                {
+                    Debug.LogError("Failed to load items");
+                }
+
+                GameObject ie = GameObject.Instantiate(itemEvent, new Vector3(transform.position.x, transform.position.y - (renderer.bounds.size.y / 2), transform.position.z), new Quaternion()) as GameObject;
+                (ie.renderer as SpriteRenderer).sprite = (it.renderer as SpriteRenderer).sprite;
+                ie.GetComponent<ItemEvent>().m_item = it;
+                break;
+
+            case 1 :
+                player.GetComponent<PlayerControl>().GainLife();
+                break;
+        }
+
+        renderer.material.color = Color.gray;
+        this.enabled = false;
+    }
+}

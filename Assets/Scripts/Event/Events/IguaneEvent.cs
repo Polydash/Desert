58c2bc83eb11a6d4dcad5944b6ca1b144bce8cbf
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-25 18:19:06 +0100

    Added IguaneEvent

---
+++ Assets/Scripts/Event/Events/IguaneEvent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IguaneEvent : EventBase
+{
+
+    protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        Debug.Log(m_data.m_choices[choice].text);
+        if (choice == 0 || choice == 1 || choice == 2)
+        {
+            player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+
+        }
+        if (choice > 2)
+        {
+            player.GetComponent<PlayerControl>().LoseLife();
+        }
+        Destroy(gameObject);
+    }
+}

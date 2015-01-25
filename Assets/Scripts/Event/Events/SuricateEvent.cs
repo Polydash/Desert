517aacaf436f173d14f137117e12950929aa805a
Polydash <polydash.dev@gmail.com>
2015-01-25 16:51:00 +0100

    Added suricate event

---
+++ Assets/Scripts/Event/Events/SuricateEvent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuricateEvent : EventBase
+{
+	protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        switch(choice)
+        {
+            case 0 :
+                GameObject itemEvent = Resources.Load<GameObject>("Prefabs/Events/ItemEvent");
+                GameObject meat = Resources.Load<GameObject>("Prefabs/Items/Raw Meat");
+                Item it = meat.GetComponent<Item>();
+                if(!it)
+                {
+                    Debug.LogError("Failed to load items");
+                }
+                player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+                GameObject ie = GameObject.Instantiate(itemEvent, new Vector3(transform.position.x, transform.position.y - (renderer.bounds.size.y / 2), transform.position.z), new Quaternion()) as GameObject;
+                (ie.renderer as SpriteRenderer).sprite = (it.renderer as SpriteRenderer).sprite;
+                ie.GetComponent<ItemEvent>().m_item = it;
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+}

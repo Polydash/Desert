70c4ccb745f472e6d80086041339cec8eab7d029
Polydash <polydash.dev@gmail.com>
2015-01-25 16:59:50 +0100

    Added Fertile Ground Event

---
+++ Assets/Scripts/Event/Events/FertileGroundEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FertileGroundEvent : EventBase
+{
+    protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+        renderer.material.color = Color.gray;
+        this.enabled = false;
+    }
+}

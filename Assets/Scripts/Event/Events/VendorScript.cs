0fcb7297779bc7936cef84b3069e0a6f55996f92
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-25 17:08:23 +0100

    Added VendorEvent

---
+++ Assets/Scripts/Event/Events/VendorScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VendorScript : EventBase {
+
+    public Transform outputItem1;
+    public Transform outputItem2;
+    public Transform outputItem3;
+
+    protected override void DoChoice(GameObject player, Item item, int itemIndex, int choice)
+    {
+        Debug.Log(m_data.m_choices[choice].text);
+        if (choice == 0)
+        {
+            player.GetComponent<PlayerControl>().LoseLife();
+            GameObject itemEvent = Resources.Load<GameObject>("Prefabs/Events/ItemEvent");
+            Item it = outputItem3.GetComponent<Item>();
+            if (!it)
+            {
+                Debug.LogError("Failed to load items");
+            }
+            GameObject ie = GameObject.Instantiate(itemEvent, new Vector3(transform.position.x, transform.position.y - (renderer.bounds.size.y / 2), transform.position.z), new Quaternion()) as GameObject;
+            (ie.renderer as SpriteRenderer).sprite = (it.renderer as SpriteRenderer).sprite;
+            ie.GetComponent<ItemEvent>().m_item = it;
+           
+        }
+        if (choice == 1 || choice == 2)
+        {
+            player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+            player.GetComponent<PlayerInventory>().SetItem(itemIndex, outputItem2.GetComponent<Item>());
+        }
+        if (choice > 2)
+        {
+            player.GetComponent<PlayerInventory>().ConsumeItem(itemIndex);
+            player.GetComponent<PlayerInventory>().SetItem(itemIndex, outputItem1.GetComponent<Item>());
+        }
+        renderer.material.color = Color.gray;
+        this.enabled = false;
+    }
+}

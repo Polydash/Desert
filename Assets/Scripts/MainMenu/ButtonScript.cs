626234e913f835817bbf712f3b0334ac4fcc651a
Cyril Malvaux <cyril.malvaux@hotmail.com>
2015-01-25 02:53:41 +0100

    Added Menu Scenes

---
+++ Assets/Scripts/MainMenu/ButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+    public Sprite normalTexture;
+    public Sprite highLightedTexture;
+    public bool Selected = false;
+    public SpriteRenderer spriteRenderer;
+
+    private bool wasSelected = false;
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        wasSelected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Selected && !wasSelected)
+        {
+            spriteRenderer.sprite = highLightedTexture;
+            wasSelected = true;
+        }
+        else if (!Selected && wasSelected)
+        {
+            spriteRenderer.sprite = normalTexture;
+            wasSelected = false;
+        }
+	}
+}
---
+++ Assets/Scripts/MainMenu/MenuScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MenuScript : MonoBehaviour {
+
+    private List<ButtonScript> buttons;
+    private int selectedButton;
+    private int lastDirection;
+	void Start () {
+        buttons = new List<ButtonScript>(GetComponentsInChildren<ButtonScript>());
+        buttons = buttons.OrderBy(t => t.transform.position.y).ToList(); ;
+        if (buttons.Count > 0)
+        {
+            buttons[0].Selected = true;
+            selectedButton = 0;
+        }
+        for (int i = 1; i < buttons.Count; i++)
+        {
+            buttons[i].Selected = false;
+        }
+        lastDirection = 0;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        int directionY = Mathf.RoundToInt(Input.GetAxis("P1 Vertical"));
+        Debug.Log(directionY);
+        if (directionY != lastDirection)
+        {
+            buttons[selectedButton].Selected = false;
+            selectedButton = (selectedButton + directionY) % (buttons.Count);
+            if (selectedButton == -1)
+                selectedButton = buttons.Count - 1;
+            buttons[selectedButton].Selected = true;
+            lastDirection = directionY;
+        }
+	}
+}
